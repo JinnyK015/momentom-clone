8b79743babf1190a30b846b4c17799001d728fa6
JinnyK015 <79676812+JinnyK015@users.noreply.github.com>
2021-06-23 21:34:49 +1200

    todolist basic finished

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const screen = document.querySelector("body");
 
 
 
+const ORIGIN_COLOR = " #e85697";
+const ORI_IMG= "linear-gradient(0deg, #e85697 0%, #a574d1 50%, #2B86C5 100%)";
 
-const ORIGIN_COLOR = "rgb(4, 207, 92)";
-const MIDDLE_COLOR =  "rgb(26, 110, 228)";
+const MIDDLE_COLOR =  "#4158D0";
+const MID_IMG = "linear-gradient(0deg, #4158D0 0%, #C850C0 30%, #FFCC70 66%, #ffffff 100%)";
+
 const LARGE_COLOR = "rgb(244, 184, 149)";
-
-
+const LRG_IMG = "linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)";
 
 const masterhandle = {
 
@@ -18,20 +20,21 @@ const masterhandle = {
        const currentScreenSize = window.innerWidth;
        if ( 700 >= currentScreenSize){
            screen.style.backgroundColor = ORIGIN_COLOR;
+           screen.style.backgroundImage = ORI_IMG;
        }
        else if (currentScreenSize >= 700 && 1200 >= currentScreenSize){
            screen.style.backgroundColor = MIDDLE_COLOR;
+           screen.style.backgroundImage = MID_IMG;
        } else {
            screen.style.backgroundColor = LARGE_COLOR;
+           screen.style.backgroundImage =LRG_IMG;
        }
     }
 }
 
 
 
-function init () {
-    screen.style.backgroundColor = ORIGIN_COLOR;
-    window.addEventListener("resize", masterhandle.handleresize);
-};
 
-init(); 
+    window.addEventListener("resize", masterhandle.handleresize);
+
+
--- todolist.js
+++ todolist.js
@@ -1,49 +1,147 @@
 const todoForm= document.querySelector('.todoForm');
 const todoInput= todoForm.querySelector('input');
 const penUl= document.getElementById('pending');
+const finUl= document.getElementById('finished');
 
-const PENDING_LS= 'localPen';
-const localpending=[];
+const PENDING_LS='pendingtodo';
+const FINISHED_LS='finishedtodo';
+
+let pendingItem =[];
+let finishedItem=[];
+
+function savePending(){
+    localStorage.setItem(PENDING_LS, JSON.stringify(pendingItem));
+}
+function saveFinished(){
+    localStorage.setItem(FINISHED_LS, JSON.stringify(finishedItem));
+}
 
 function deletePending(event){
     const li = event.target.parentElement;
     li.remove();
+    pendingItem = pendingItem.filter((toDo) => toDo.id !== parseInt(li.id));
+    savePending();
 }   
 
+function deleteFinished(event){
+    const li = event.target.parentElement;
+    li.remove();
+    finishedItem = finishedItem.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveFinished();
+}   
 
-function printtodo(text){
+function moveFinished(event){
+    const li = event.target.parentElement;
+ 
+    const liObj={
+        text:li.querySelector("span").innerHTML,
+        id: Date.now()
+    };
+
+    finishedItem.push(liObj);    
+    printFin(liObj);
+    saveFinished(liObj);
+
+    penUl.removeChild(li);
+    pendingItem = pendingItem.filter((toDo) => toDo.id !== parseInt(li.id));
+    savePending(pendingItem);
+}
+
+function movePending(event){
+    const li = event.target.parentElement;
+ 
+    const liObj={
+        text:li.querySelector("span").innerHTML,
+        id: Date.now()
+    };
+
+    pendingItem.push(liObj);    
+    printPen(liObj);
+    savePending(liObj);
+
+    finUl.removeChild(li);
+    finishedItem = finishedItem.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveFinished(finishedItem);
+}
+
+
+
+function printFin(newItem){
     li = document.createElement('li');
-    delbtn = document.createElement('button');
-    span =document.createElement('span');
+    li.id =newItem.id;
 
+    penbtn = document.createElement('button');
+    penbtn.innerHTML = 'Undo';
+    penbtn.addEventListener('click', movePending);
+
+    delbtn = document.createElement('button');
     delbtn.innerHTML = 'Del';
-    delbtn.addEventListener('click', deletePending);
-    span.innerHTML=text;
-    
+    delbtn.addEventListener('click', deleteFinished);
+
+    span = document.createElement('span');
+ 
+
+    span.innerHTML=newItem.text;
 
     li.appendChild(span);
+    li.appendChild(penbtn);
     li.appendChild(delbtn);
 
+    finUl.appendChild(li);
+}
+
+
+
+function printPen(newItem){
+    li = document.createElement('li');
+    li.id =newItem.id;
+
+    finbtn = document.createElement('button');
+    finbtn.innerHTML = 'Done';
+    finbtn.addEventListener('click', moveFinished);
+
+    delbtn = document.createElement('button');
+    delbtn.innerHTML = 'Del';
+    delbtn.addEventListener('click', deletePending);
+
+    span = document.createElement('span');
+ 
+
+    span.innerHTML=newItem.text;
+
+    li.appendChild(span);
+    li.appendChild(finbtn);
+    li.appendChild(delbtn);
 
     penUl.appendChild(li);
-            
-    let dataid = new Date();
-    let newID= dataid++;
-
-    const liObj={
-        text: text,
-        id: newID
-    }
-    console.log(liObj);
-   
 }
 
 function handleSubmit(event){
     event.preventDefault();
-    printtodo(todoInput.value);
+    const newItem =todoInput.value;
     todoInput.value="";
+    const liObj={
+        text: newItem,
+        id: Date.now()
+    };
+    pendingItem.push(liObj);    
+    printPen(liObj);
+    savePending(liObj);
 }
 
+const pendingtodo = localStorage.getItem(PENDING_LS);
+const finishedtodo = localStorage.getItem(FINISHED_LS);
 
+if(pendingtodo !== null){
+    const parsedToDos = JSON.parse(pendingtodo);
+    pendingItem = parsedToDos;
+     parsedToDos.forEach(printPen);
+}
+
+if(finishedtodo !== null){
+    const parsedFinshed = JSON.parse(finishedtodo);
+    finishedItem = parsedFinshed;
+    parsedFinshed.forEach(printFin);
+}
 
 todoForm.addEventListener('submit', handleSubmit);
